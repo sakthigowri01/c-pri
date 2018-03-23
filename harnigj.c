448337b183a02c195aaf426ab259c4c9540d7c89
sakthigowri01 <35915299+sakthigowri01@users.noreply.github.com>
2018-03-23 15:54:48 +0530

    harnigj.c

---
+++ harnigj.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include<stdio.h>
+int main(void) {
+	int n,m,i,count=0;
+	scanf("%d %d",&n,&m);
+	int a[15];
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		if(a[i]==m)
+		{
+			
+			count=count+1;
+		
+		}
+		
+	}
+	printf("%d",count);
+	return 0;
+}
