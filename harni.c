828d2aeb1e006385a5e73c261e6373d978a088c2
sakthigowri01 <35915299+sakthigowri01@users.noreply.github.com>
2018-03-23 15:44:28 +0530

    harni.c

---
+++ harni.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<conio.h>
+int main(void) {
+	int n,count=1;
+	scanf("%d",&n);
+	while(n!=0)
+	{
+		n=n/20;
+		if(n!=0)
+		{
+			count=count+1;
+		}
+	}
+	printf("%d",count);
+	return 0;
+}
